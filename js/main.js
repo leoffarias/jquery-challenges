ea40dec502eb273ef4df28200539633aaba3018e
Leandro Farias <lferrazfarias@albany.edu>
2014-11-25 19:05:00 -0500

    challenge 2 incomplete

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-  $("#btn-menu").click(function(){
-    $(".nav-main").slideToggle("slow");
-  });
+	//challenge 1
+	$("#btn-menu").click(function(){
+		$(".nav-main").slideToggle("slow");
+	});
+
+	//challenge 2
+	$(".panel-title").click(function(){
+		$(this).next().slideToggle("slow");
+		$(this).next().siblings().slideDown("slow");
+	});
 });
